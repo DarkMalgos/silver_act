13b004e40d67712a09e1876699377f17717908ed
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-02-22 17:34:36 +0100

    structure home qui sommenous? comment ça marche?

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -1,46 +1,22 @@
-$(function(){
-    $(window).scroll(function(){
-        scrollVal=$(window).scrollTop();
-        if(scrollVal>250) {
-                $('header nav>ul').removeClass('center-col');   
-                $('header nav').css({
-                    'transition':'all ease 1s',
-                    'position':'fixed',
-                    'top':'0px'
-                });
-                $('header nav>ul>li:first-child').css({
-                    'transition':'all ease 1s',
-                    'display':'block'
-                });
-            } else {
-                $('header nav>ul').addClass('center-col');   
-                $('header nav').css({
-                    'transition':'all ease 1s',
-                    'position':'relative',
-                    'top':'0px'
-                });
-                $('header nav>ul>li:first-child').css({
-                    'transition':'all ease 1s',
-                    'display':'none'
-                }); 
-            } 
-    });
-});
+window.onscroll = function toto() {
+    if ($('body').scrollTop() < 979){
+        console.log('ouiiiii');
+    }
+    else if ($('body').scrollTop() >= 980) {
+        console.log('toto');
+    } 
+}
 var titi;
 
-function toto() {
-    
-    $('#sticky input[type=tel]').click(function() {
-        console.log('fix');
-        $('#sticky').css({
-            'right': '0px',
-            'transition': 'all ease 1s'
-        });
-        console.log('after');
+$('#sticky input[type=tel]').click(function() {
+    console.log('fix');
+    $('#sticky').css({
+        'right': '0px',
+        'transition': 'all ease 1s'
+    });
+    console.log('after');
 });
-}
 
-toto();
 
 $('#sticky input[type=submit]').click(function(e) {
     e.preventDefault();
