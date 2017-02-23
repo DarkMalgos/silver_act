7cbd3007ea4e08ee08cda94edf539d680a2f3b77
fazeo <ngouma.r@gmail.com>
2017-02-23 13:49:26 +0100

    init

--- assets/js/qui-sommes-nous.js
+++ assets/js/qui-sommes-nous.js
@@ -1,49 +1,36 @@
-var titi;
+$(function(){
+    
+     $(window).scroll(function(){
+        scrollVal=$(window).scrollTop();
+              
+         
+         
+         if(scrollVal>250){
+             
+            
+            $('header nav>ul').removeClass('center-col');   
+            $('header nav').css({'transition':'all ease 1s','position':'fixed','top':'0px'});
+            $('header nav>ul>li:first-child').css({'transition':'all ease 1s','display':'block'});
 
-$('#sticky input[type=tel]').click(function() {
-    console.log('fix');
-    $('#sticky').css({
-        'right': '0px',
-        'transition': 'all ease 1s'
-    });
-    console.log('after');
-});
+        }
+        
+         if(scrollVal>900){
+             
+            
+//            $('#sticky').css({'right':'-800px'}); 
 
-
-$('#sticky input[type=submit]').click(function(e) {
-    e.preventDefault();
-    $('#sticky form').css({
-        'display': 'none'
-    });
-    $('#send').css({
-        'display': 'inline-block'
-    });
-    titi = setTimeout(wait_third_sec, 3000);
-});
-
-function wait_third_sec() {
-    $('#sticky').css({
-        'right': '-730px',
-        'transition': 'all ease 1s'
-    });
-    $('#sticky form').css({
-        'display': 'flex'
-    });
-    $('#send').css({
-        'display': 'none'
-    });
-    $('#sticky').hover(function() {
-        console.log('in');
-        $('#sticky').css({
-            'right': '0px',
-            'transition': 'all ease 1s'
+        }
+         
+        else{
+          $('header nav>ul').addClass('center-col');   
+            $('header nav').css({'transition':'all ease 1s','position':'relative','top':'0px'});
+            $('header nav>ul>li:first-child').css({'transition':'all ease 1s','display':'none'});  
+              $('#sticky').css({'right':'-730px'}); 
+        } 
+         
         });
-    }, function() {
-        console.log('out');
-        $('#sticky').css({
-            'right': '-730px',
-            'transition': 'all ease 1s'
-        });
-    });
-    clearTimeout(titi);
-}
+         
+  
+    
+    
+});
