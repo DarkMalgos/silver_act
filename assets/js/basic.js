7b2d6603843c082eb015b5d262436bddef5b57d1
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-02-28 13:19:28 +0100

    js: nav second

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -26,3 +26,55 @@ function wait_third_sec() {
     });
     clearTimeout(titi);
 }
+
+window.onscroll = function () {
+    $('body').bind('DOMMouseScroll', function(e) {
+        console.log('ta mere');
+        if (e.originalEvent.detail < 0) {
+            console.log($('body').scrollTop())
+            if ($('body').scrollTop() > 160) {
+                console.log('ta soeur')
+                if ($('#header-move').css('display') == 'none') {
+                    $('#header-move').css({
+                        display : 'block',
+                        opacity : '1',
+                        transition : 'all ease 1s'
+                    });
+                }
+            } else {
+                if ($('#header-move').css('display') == 'block') {
+                    $('#header-move').css({
+                        display : 'none',
+                        opacity : '0',
+                        transition : 'all ease 1s'
+                    });
+                }
+            }
+        }
+    });
+    //IE, Opera, Safari, Chrome
+    $('body').bind('mousewheel', function(e) {
+        console.log($('body').scrollTop());
+        if (e.originalEvent.wheelDelta > 0) {
+            if ($('body').scrollTop() > 160) {
+                if ($('#header-move').css('display') == 'none') {
+                    $('#header-move').css({
+                        display : 'block',
+                        opacity : '1',
+                        transition : 'all ease 1s'
+                    });
+                }
+            } else {
+                if ($('#header-move').css('display') == 'block') {
+                    $('#header-move').css({
+                        display : 'none',
+                        opacity : '0',
+                        transition : 'all ease 1s'
+                    });
+                }
+            }
+        }
+    });
+    //Firefox
+    
+}
