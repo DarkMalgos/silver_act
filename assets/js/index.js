9b01a79cd321efc86aee19ee27717daf97f2e8d5
mehdi Aniq-Filali <mehdi Aniq-Filali>
2017-03-10 22:38:27 +0100

    questions frequentes sans js

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+var swiper1 = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    Spacebetween: 10,
+    effet: 'fade'
+});
+
+var swiper2 = new Swiper('#comments', {
+    pagination: '#s-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    spaceBetween: 'auto',
+    
+    effet: 'fade'
+});
+
